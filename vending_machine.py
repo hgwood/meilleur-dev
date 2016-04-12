3f2589d99a6a52da67c7d6c1b258c1d5433add11
Hugo Wood <hx4.5@free.fr>
2016-04-12 15:12:46 +0200

    refactor: improved readability

--- vending_machine.py
+++ vending_machine.py
@@ -1,37 +1,33 @@
+def minimum_number_of_coins(amount, wallet):
+    solutions = [(0, wallet)]
+    for intermediate_amount in range(1, amount + 1):
+        solutions.append(find_best_solution(intermediate_amount, wallet, solutions))
+    return solutions[-1][0]
+
+def find_best_solution(amount, coin_values, solutions):
+    return min(
+        find_solutions(amount, coin_values, solutions), 
+        key=lambda solution: solution[0], 
+        default=("IMPOSSIBLE", {}))
+
+def find_solutions(amount, coin_values, solutions):
+    for coin_value in coin_values:
+        if coin_value <= amount:
+            coins_given, wallet = solutions[amount - coin_value]
+            if wallet.get(coin_value, 0) > 0:
+                yield coins_given + 1, remove_coin(wallet, coin_value)
+
+def remove_coin(wallet, coin_value):
+    return update(wallet, {coin_value: wallet[coin_value] - 1})
+
 def update(a_dict, another_dict):
     new_dict = a_dict.copy()
     new_dict.update(another_dict)
     return new_dict
 
-def remove_coin(wallet, coin_value):
-    return update(wallet, {coin_value: wallet[coin_value] - 1})
 
-def has_next_solution(solution, next_coin_value):
-    coins_given, wallet = solution
-    return wallet[next_coin_value] > 0 and coins_given != "IMPOSSIBLE"
-
-def next_solution(solution, next_coin_value):
-    coins_given, wallet = solution
-    return coins_given + 1, remove_coin(wallet, next_coin_value)
-    
-def parent_solutions(solutions, amount_left, coin_values):
-    for coin_value in coin_values:
-        if coin_value <= amount_left:
-            solution = (solutions[amount_left - coin_value], coin_value)
-            if has_next_solution(*solution):
-                yield next_solution(*solution)
-
-def best_parent_solution(solutions, amount_left, coin_values):
-    return min(parent_solutions(solutions, amount_left, coin_values), key=lambda solution: solution[0], default=("IMPOSSIBLE", {}))
-
-def minimum_number_of_coins(amount, wallet):
-    solutions = [(0, wallet)]
-    for i in range(1, amount + 1):
-        solutions.append(best_parent_solution(solutions, i, wallet))
-    return solutions[-1][0]
-
-amount = int(input())
-ncoin_types = int(input())
-wallet = {coinValue: coinsLeft for coinsLeft, coinValue in (map(int, input().split()) for _ in range(ncoin_types))}
-
-print(minimum_number_of_coins(amount, wallet))
+if __name__ == "__main__":
+    amount = int(input())
+    ncoin_types = int(input())
+    wallet = {coin_value: coins_left for coins_left, coin_value in (map(int, input().split()) for _ in range(ncoin_types))}
+    print(minimum_number_of_coins(amount, wallet))
